470fdbec3b1375404a7be0cf02fe5fba2cfa1987
wongco <ginson@gmail.com>
2018-11-30 12:43:54 -0800

    part 1 html

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
+from flask_debugtoolbar import DebugToolbarExtension
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 db.create_all()
+
+# @app.route("/")
+# def list_pets():
+#     """List pets and show add form."""
+
+#     pets = Pet.query.all()
+#     return render_template("list.html", pets=pets)
+
+
+# @app.route("/", methods=["POST"])
+# def add_pet():
+#     """Add pet and redirect to list."""
+
+#     name = request.form.get('name')
+#     species = request.form.get('species')
+#     hunger = request.form.get('hunger')
+#     hunger = int(hunger) if hunger else None
+
+#     pet = Pet(name=name, species=species, hunger=hunger)
+#     db.session.add(pet)
+#     db.session.commit()
+
+#     return redirect("/")
+
+
+# @app.route("/<int:pet_id>")
+# def show_pet(pet_id):
+#     """Show info on a single pet."""
+
+#     pet = Pet.query.get_or_404(pet_id)
+#     return render_template("detail.html", pet=pet)
--- models.py
+++ models.py
@@ -1,1 +1,52 @@
+from flask_sqlalchemy import SQLAlchemy
 """Models for Blogly."""
+"""Demo file showing off a model for SQLAlchemy."""
+
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(25),
+                           nullable=False)
+    last_name = db.Column(db.String(25),
+                          nullable=False)
+    image_url = db.Column(db.Text,
+                          nullable=True, default="https://vignette.wikia.nocookie.net/sote-rp/images/c/c4/User-placeholder.png/revision/latest?cb=20150624004222")
+
+    # def greet(self):
+    #     """Greet using name."""
+
+    #     return f"I'm {self.name} the {self.species or 'thing'}"
+
+    # def feed(self, units=10):
+    #     """Nom nom nom."""
+
+    #     self.hunger -= units
+    #     self.hunger = max(self.hunger, 0)
+
+    # def __repr__(self):
+    #     """Show info about pet."""
+
+    #     p = self
+    #     return f"<Pet {p.id} {p.name} {p.species} {p.hunger}>"
+
+    # @classmethod
+    # def get_by_species(cls, species):
+    #     """Get all pets matching that species."""
+
+    #     return cls.query.filter_by(species=species).all()
